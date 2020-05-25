9914e9e0c1fffce95c57e5eca0301e3bc5f497e5
adrianrivers <adrianlrivers@gmail.com>
2020-05-25 18:24:24 +0200

    Add body comp table component

---
+++ src/components/BodyCompositionTable.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let bodyFatPercentage;
+  export let fatMass;
+  export let leanMass;
+</script>
+
+<style></style>
+
+<table>
+ <tr>
+  <th>Body Fat %</th>
+  <th>Fat Mass</th>
+  <th>Lean Mass</th>
+ </tr>
+ <tr>
+  <td>{bodyFatPercentage}</td>
+  <td>{fatMass}</td>
+  <td>{leanMass}</td>
+ </tr>
+</table>
