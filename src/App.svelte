7d0623a9fbec646f9d58b1114ceb958e7b53e6d3
adrianrivers <adrianlrivers@gmail.com>
2020-05-25 23:13:47 +0200

    Incorporate chart.js lib, tester chart component

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,21 @@
 <script>
-	import { onMount } from 'svelte';
 	import { data, legacyData } from '../data';
 	import { transformData } from './utils/transform-data.js'
 	import BodyCompositionTable from './components/BodyCompositionTable.svelte';
+	import BodyCompositionChart from './components/BodyCompositionChart.svelte';
 
 	let bodyCompositionData = transformData(data, legacyData);
 </script>
 
 <main>
+	<BodyCompositionChart bodyCompositionData={bodyCompositionData} />
 	<BodyCompositionTable bodyCompositionData={bodyCompositionData} />
 </main>
 
 <style>
+	main {
+		margin: 3em;
+		max-width: 1400px;
+		margin: 0 auto;
+	}
 </style>
